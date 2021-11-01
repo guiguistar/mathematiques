0221a2827a1519262a3d4ac497e344d926cfc354
Guillaume Roux <guiguionthenet@free.fr>
2021-11-01 16:13:46 +0100

    L'ensemble des parties d'un ensemble.

--- ensembles.py
+++ ensembles.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 13 15:08:12 2020
 
-@author: roux
-"""
-
+class Ensemble(frozenset):
+    def __new__(cls, *elements):
+        return super().__new__(cls, elements)
+    def __repr__(self):
+        return "{" + ", ".join(map(str,self)) + "}"
+    def __and__(self, autre):
+        return Ensemble(*frozenset.intersection(self, autre))
+    def __or__(self, autre):
+        return Ensemble(*frozenset.union(self, autre))
+    def __sub__(self, autre):
+        return Ensemble(*frozenset.difference(self, autre))
+    def intersection(self, autre):
+        return Ensemble(*frozenset.intersection(self, autre))
+    def union(self, autre):
+        return Ensemble(*frozenset.union(self, autre))
+    def difference(self, autre):
+        return Ensemble(*frozenset.difference(self, autre))
+    def parties(self):
+        p = []
+        liste = list(self)
+        n = len(liste)
+        for i in range(2**n):
+            p_ = []
+            for j in range(n):
+                if (1 << j) & i:
+                    p_.append(liste[j])
+            p.append(Ensemble(*p_))
+        print(p)
+        return Ensemble(*p)
 def ensemble(*args):
     return set(args)
 
@@ -36,4 +60,11 @@ if __name__ == '__main__':
     #x = produit_cartesien
     #print(x(A,x(B,C)))
     
-    print(*produit(A,B,C,D),sep='\n')
+    #print(*produit(A,B,C,D),sep='\n')
+
+    a = Ensemble(4,2,45,42)
+    b = Ensemble('a','b')
+    c = Ensemble(a,b)
+    d = Ensemble(4,3)
+    print(a, b, c, d, a.intersection(d), a.union(b), Ensemble(a))
+    print(a.parties())
