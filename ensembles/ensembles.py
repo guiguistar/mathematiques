2232b7c1258ac843bea1480f761936f3f8933e9c
Guillaume Roux <guiguionthenet@free.fr>
2021-11-03 20:27:56 +0100

    Suppression des fonctions obsolètes.

--- ensembles/ensembles.py
+++ ensembles/ensembles.py
@@ -50,25 +50,5 @@ class Ensemble(frozenset):
 def adjonction(X, a):
     return Ensemble(*map(lambda ens : ens|Ensemble(a), X))
 
-def ensemble(*args):
-    return set(args)
-
-def ensemble_iterable(*args):
-    return set((a,) for a in args)
-
-def rendre_iterable(ensemble):
-    return set((x,) for x in ensemble)
-
-def produit_cartesien(A,B):
-    return set((a,b) for a in A for b in B)
-
-def produit(*ensembles):
-    print(len(ensembles))
-    if len(ensembles) == 2:
-        return produit_cartesien(*ensembles)
-    else:
-        *ensembles, dernier = ensembles
-        return produit_cartesien(produit(*ensembles), dernier)
-
 Card = len
 P = lambda e : Ensemble.parties(e)
